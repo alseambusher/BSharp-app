a4b6cea1884a7e86f839e9b4018c2b96df9ac2cd
Jashwanth Reddy Madem <jswt001@gmail.com>
2016-04-30 02:33:03 -0700

    basic electron app

--- index.js
+++ index.js
@@ -2,48 +2,11 @@
 const electron = require('electron');
 const app = electron.app;
 
+//var reader = require('midi-reader');
+
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
-
-
-var midi = require('midi');
-
-// Set up a new input.
-var input = new midi.input();
-
-// Count the available input ports.
-input.getPortCount();
-
-// Get the name of a specified input port.
-input.getPortName(0);
-
-// Configure a callback.
-input.on('message', function(deltaTime, message) {
-  // The message is an array of numbers corresponding to the MIDI bytes:
-  //   [status, data1, data2]
-  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
-  // information interpreting the messages.
-  console.log('m:' + message + ' d:' + deltaTime);
-});
-
-// Open the first available input port.
-input.openPort(0);
-
-// Sysex, timing, and active sensing messages are ignored
-// by default. To enable these message types, pass false for
-// the appropriate type in the function below.
-// Order: (Sysex, Timing, Active Sensing)
-// For example if you want to receive only MIDI Clock beats
-// you should use
-// input.ignoreTypes(true, false, true)
-input.ignoreTypes(false, false, false);
-
-// ... receive MIDI messages ...
-
-// Close the port when done.
-//input.closePort();
-// prevent window being garbage collected
 let mainWindow;
 
 function onClosed() {
@@ -64,11 +27,7 @@ function createMainWindow() {
 	return win;
 }
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
+app.on('window-all-closed', app.quit);
 
 app.on('activate', () => {
 	if (!mainWindow) {
---
+++ midi-reader.js
@@ -0,0 +1,27 @@
+var midi = require('midi');
+
+// Set up a new input.
+exports.input = new midi.input();
+
+// Count the available input ports.
+input.getPortCount();
+
+// Get the name of a specified input port.
+input.getPortName(0);
+
+// Configure a callback.
+input.on('message', function(deltaTime, message) {
+  console.log('m:' + message + ' d:' + deltaTime);
+});
+
+// Open the first available input port.
+input.openPort(0);
+
+input.ignoreTypes(false, false, false);
+
+// ... receive MIDI messages ...
+
+// Close the port when done.
+//input.closePort();
+// prevent window being garbage collected
+
