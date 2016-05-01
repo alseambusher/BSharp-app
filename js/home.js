259642af959c4c39bacdcbf0353f7e06ff5ba4f4
Jashwanth Reddy Madem <jswt001@gmail.com>
2016-04-30 19:51:48 -0700

    learning mode

--- js/home.js
+++ js/home.js
@@ -12,7 +12,7 @@ exports.interrupt = true;
 
 exports.init = function() {
 	reader.start(function(deltaTime, message) {
-		console.log(message, deltaTime);
+		//console.log(message, deltaTime);
 		m = message.toString().split(",");
 		exports.song.push([parseInt(m[0]),parseInt(m[1]),parseInt(m[2]), parseFloat(deltaTime)]);
 		});
@@ -34,12 +34,16 @@ exports.init = function() {
  		midi_start_record_expert();
 	});
 
-	//ipc.on("midi-start-record
+	ipc.on("midi-start-record-learner", () => {
+		exports.interrupt = false;
+		exports.song = [];
+		midi_start_record_learner(undefined);
+	});
 
 	ipc.on("midi-stop-record", () => {
 		//reader.stop();
 		exports.interrupt = true;
-	    	exports.strip.color("#000");
+	    	//exports.strip.color("#000");
 	});
 
 }
@@ -47,7 +51,6 @@ exports.init = function() {
 function midi_start_record(){
 	if (exports.interrupt) {
 		exports.song_out = exports.song.slice(0);
-		console.log(exports.song_out)
 	} else
 		setTimeout(midi_start_record, 10);
 }
@@ -69,16 +72,32 @@ function midi_start_record_simple(){
 
 function midi_start_record_learner(wait){
 	if (!exports.interrupt){
-		if (exports.song_out.length > 0){
-			node = exports.song_out.shift();
-			key = node[1] - 28;
-			if (node[0] != 128){
-				led.strip.pixel(key).color("green");
-				//if (
+		if (wait == undefined){
+			if (exports.song_out.length > 0){
+				node = exports.song_out.shift();
+				key = node[1] - 28;
+				if (node[0] != 128){
+					led.strip.pixel(key).color("green");
+					wait = key;
+				}
+				led.strip.show();
 			}
-			led.strip.show();
+		} else {
+			if (exports.song.length > 0){
+				node = exports.song.shift();
+				key = node[1] - 28;
+				if (node[0] != 128){
+					if (wait != key)
+						led.strip.pixel(key).color("red");
+					else{
+						led.strip.pixel(key).color("#000");
+						wait = undefined;
+					}
+				}
+				led.strip.show();
+			}
 		}
-		setTimeout(midi_start_record_learner, 10);
+		setTimeout( function() { midi_start_record_learner(wait)}, 10);
 	}
 }
 
