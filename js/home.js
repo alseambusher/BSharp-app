b0667570c54b62ad7fa92c826320abd828de9365
Jashwanth Reddy Madem <jswt001@gmail.com>
2016-05-01 05:16:21 -0700

    proto

--- js/home.js
+++ js/home.js
@@ -2,13 +2,20 @@ const electron = require("electron");
 const ipc = electron.ipcMain;
 var reader = require("./midi-reader.js");
 var led = require("./led.js");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 exports.mode = 0; // 0 - simple, 1 - learner, 2 - expert
 
 exports.song = [];
-exports.song_out =[[144,30,25,0], [144, 31,31,0.1], [144, 32,32,0.9]];
+exports.song_out =[];
 
 exports.interrupt = true;
+
+colors = ["red", "green", "blue", "yellow", "cyan"];
+bcolors = ['cyan', 'yellow', 'blue', 'green', 'red'];
+
+var white = {9: 0,11: 0,13: 0,14: 0,16: 0,20: 0,21: 1,23: 1,25: 1,26: 1,28: 1,32: 1,33: 2,35: 2,37: 2,38: 2,40: 2,44: 2,45: 3,47: 3,49: 3,50: 3,52: 3,56: 3,57: 4,59: 4,61: 4,62: 4,64: 4,68: 4}
+var black = {10: 0,12: 0,15: 0,17: 0,19: 0,22: 1,24: 1,27: 1,29: 1,31: 1,34: 2,36: 2,39: 2,41: 2,43: 2,46: 3,48: 3,51: 3,53: 3,55: 3,58: 4,60: 4,63: 4,65: 4,67: 4}
 
 exports.init = function() {
 	reader.start(function(deltaTime, message) {
@@ -29,9 +36,9 @@ exports.init = function() {
  		midi_start_record_simple();
 	});
 
-	ipc.on("midi-start-record-expert", () => {
+	ipc.on("midi-start-record-expert", (event, speed) => {
 		exports.interrupt = false;
- 		midi_start_record_expert();
+ 		midi_start_record_expert(speed);
 	});
 
 	ipc.on("midi-start-record-learner", () => {
@@ -43,7 +50,16 @@ exports.init = function() {
 	ipc.on("midi-stop-record", () => {
 		//reader.stop();
 		exports.interrupt = true;
-	    	//exports.strip.color("#000");
+	});
+
+	ipc.on("midi-load-song", (event, song) => {
+		console.log(song);
+		exports.song_out = song;
+	});
+
+	ipc.on("clear-keyboard", () => {
+	    	led.strip.color("#000");
+		led.strip.show();
 	});
 
 }
@@ -51,6 +67,16 @@ exports.init = function() {
 function midi_start_record(){
 	if (exports.interrupt) {
 		exports.song_out = exports.song.slice(0);
+		if (exports.song_out.length > 0){
+   			xmlhttp = new XMLHttpRequest();
+   			xmlhttp.open("POST","http://b-sharp.co/store", true);
+   			xmlhttp.onreadystatechange=function(){
+         			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+           				string=xmlhttp.responseText;
+         			}
+   			}
+   			xmlhttp.send(JSON.stringify(exports.song_out));
+		}
 	} else
 		setTimeout(midi_start_record, 10);
 }
@@ -62,8 +88,17 @@ function midi_start_record_simple(){
 			key = node[1] - 28;
 			if (node[0] == 128)
 				led.strip.pixel(key).color("black");
-			else
-				led.strip.pixel(key).color("green");
+			else{
+				if (white[key]){
+					led.strip.pixel(key).color(colors[white[key]]);
+				}
+				else if(black[key]){
+					led.strip.pixel(key).color(bcolors[black[key]]);
+				}
+				else{
+					led.strip.pixel(key).color("white");
+				}
+			}
 			led.strip.show();
 		}
 		setTimeout(midi_start_record_simple, 10);
@@ -101,18 +136,30 @@ function midi_start_record_learner(wait){
 	}
 }
 
-function midi_start_record_expert(){
+function midi_start_record_expert(speed){
 	if (!exports.interrupt){
 		if (exports.song_out.length > 0){
 			node = exports.song_out.shift();
 			key = node[1] - 28;
-			if (node[0] == 128)
+			if (node[0] == 128){
 				led.strip.pixel(key).color("black");
-			else
-				led.strip.pixel(key).color("green");
+			}
+			else{
+				if (white[key]){
+					led.strip.pixel(key).color(colors[white[key]]);
+				}
+				else if(black[key]){
+					led.strip.pixel(key).color(bcolors[black[key]]);
+				}
+				else{
+					led.strip.pixel(key).color("white");
+				}
+			}
 			led.strip.show();
 			if (exports.song_out.length > 0)
-				setTimeout(midi_start_record_expert, exports.song_out[0][3]*1000);
+				setTimeout( function() {
+					midi_start_record_expert(speed)
+					}, (exports.song_out[0][3]*1000)/speed);
 		}
 	}
 }
--- js/view/menu.js
+++ js/view/menu.js
@@ -2,7 +2,7 @@ var remote = require('electron').remote;
 var dialog = remote.require('dialog');
 const ipcRenderer = require('electron').ipcRenderer;
 
-var navs = ["home", "store", "tools"];
+var navs = ["home", "tools"];
 
 navs.forEach(function (e){
 	document.getElementById("nav-"+e).addEventListener('click', function(event){
@@ -16,10 +16,38 @@ navs.forEach(function (e){
 
 document.getElementById("button-midi-start").addEventListener('click', function(event){
 	mode = document.getElementById("record-mode").value;
-	ipcRenderer.send('midi-start-record-'+mode);
+	speed = parseFloat(document.getElementById("record-mode-speed").value);
+	ipcRenderer.send('midi-start-record-'+mode, speed);
 });
 
 document.getElementById("button-midi-stop").addEventListener('click', function(event){
+	mode = document.getElementById("record-mode").value;
 	ipcRenderer.send('midi-stop-record');
 });
+
+document.getElementById("button-midi-clear").addEventListener('click', function(event){
+	ipcRenderer.send('clear-keyboard');
+});
+
+function loadSong(song){
+	ipcRenderer.send('midi-load-song', song);
+}
+
+function getStore() {
+	$.get("http://b-sharp.co/store", function(data){
+		songs = JSON.parse(data);
+		console.log(songs[0].song);
+		var html = "";
+		for (var i=0; i<songs.length; i++){
+			
+			html += "<tr onclick='loadSong("+JSON.stringify(songs[i].song)+")'><td>";
+			html += songs[i].timestamp;
+			html += "</td><td>"+songs[i].song.length+"</td></tr>";
+
+		}
+		document.getElementById("store").innerHTML=html;
+	});
+}
+
+getStore();
 
